21e11aa3281d5cc3fc341aac8f27db9da0403ac6
crashahotrod <75499789+crashahotrod@users.noreply.github.com>
2023-05-15 02:13:11 -0500

    Add Kick.com Support

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 # twitch_liveleech - Copyright 2022 IRLToolkit Inc.
-# Usage: twitch_liveleech.py [channel] [dump path] [final path]
-# Modified by crashahotrod to make more plex friendly and rename logs for multi-channel support
+# Usage: main.py [mode] [channel] [dump path] [final path]
+# Modified by crashahotrod to add the following features:
+    #Make output more plex friendly
+    #Rename logs for multi-channel support
+    #Disable ads on subscribed channels
+    #Add kick.com support
 
 import sys
-channelName = sys.argv[1]
-downloadPath = sys.argv[2]
-finalPath = sys.argv[3]
+mode = sys.argv[1] #kick or twitch
+channelName = sys.argv[2]
+downloadPath = sys.argv[3]
+finalPath = sys.argv[4]
 logname= str(channelName) + ".log"
 muxlogname = str(channelName) + "_mux.log"
 downloadlogname = str(channelName) + "_download.log"
@@ -20,47 +25,73 @@ import datetime
 import requests
 import streamlink
 import ffmpeg
-
+import cloudscraper
+months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
+sleepDuration = 35
+sl = streamlink.Streamlink()
 twitchClientId = os.getenv('TWITCH_LIVELEECH_CLIENT_ID')
 twitchClientSecret = os.getenv('TWITCH_LIVELEECH_CLIENT_SECRET')
 #Disable Ads on Subscribed channels https://streamlink.github.io/cli/plugins/twitch.html#authentication
 twitchAuthorization = os.getenv('TWITCH_LIVELEECH_AUTHORIZATION')
-
-months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
-sleepDuration = 35
-
-sl = streamlink.Streamlink()
-sl.set_plugin_option('twitch', 'disable-hosting', True)
-sl.set_plugin_option('twitch', 'disable-ads', True)
-sl.set_plugin_option('twitch', 'disable-reruns', True)
 twitchAPIheader={'Authorization' : twitchAuthorization}
-sl.set_plugin_option('twitch', 'api-header', twitchAPIheader)
+
+if(mode == "twitch"):
+    sl.set_plugin_option('twitch', 'disable-hosting', True)
+    sl.set_plugin_option('twitch', 'disable-ads', True)
+    sl.set_plugin_option('twitch', 'disable-reruns', True)
+    sl.set_plugin_option('twitch', 'api-header', twitchAPIheader)
+elif(mode == "kick"):
+    pass
+else:
+    logging.critical("Invalid mode argument please specify kick or twitch")
+    os._exit(1)
 
 def append_file(fileName, data):
     with open(fileName, 'a') as f:
         f.write('\n')
         f.write(data.decode())
 
-def get_channel_title():
-    req = requests.post('https://id.twitch.tv/oauth2/token?client_id={}&client_secret={}&grant_type=client_credentials'.format(twitchClientId, twitchClientSecret))
-    if req.status_code != requests.codes.ok:
-        logging.warning('Failed to get Twitch app auth token due to HTTP error. Code: {} | Text: {}'.format(req.status_code, req.text))
-        return 'UNKNOWN TITLE'
-    twitchAuthorization = req.json()['access_token']
-    headers = {'Client-Id': twitchClientId, 'Authorization': 'Bearer ' + twitchAuthorization}
-    req = requests.get('https://api.twitch.tv/helix/users?login={}'.format(channelName.lower()), headers=headers)
-    if req.status_code != requests.codes.ok:
-        logging.warning('Failed to get Twitch user id due to HTTP error. Code: {} | Text: {}'.format(req.status_code, req.text))
-        return 'UNKNOWN TITLE'
-    channelId = req.json()['data'][0]['id']
-    req = requests.get('https://api.twitch.tv/helix/channels?broadcaster_id={}'.format(channelId), headers=headers)
-    if req.status_code != requests.codes.ok:
-        logging.warning('Failed to get channel title due to HTTP error. Code: {} | Text: {}'.format(req.status_code, req.text))
-        return 'UNKNOWN TITLE'
-    data = req.json()
-    title = data['data'][0]['title']
-    logging.info('Found Video Title: {}'.format(title))
-    return title
+def get_channel_title(old):
+    if(mode == "twitch"):
+        req = requests.post('https://id.twitch.tv/oauth2/token?client_id={}&client_secret={}&grant_type=client_credentials'.format(twitchClientId, twitchClientSecret))
+        if req.status_code != requests.codes.ok:
+            logging.warning('Failed to get Twitch app auth token due to HTTP error. Code: {} | Text: {}'.format(req.status_code, req.text))
+            return 'UNKNOWN TITLE'
+        twitchAuthorization = req.json()['access_token']
+        headers = {'Client-Id': twitchClientId, 'Authorization': 'Bearer ' + twitchAuthorization}
+        req = requests.get('https://api.twitch.tv/helix/users?login={}'.format(channelName.lower()), headers=headers)
+        if req.status_code != requests.codes.ok:
+            logging.warning('Failed to get Twitch user id due to HTTP error. Code: {} | Text: {}'.format(req.status_code, req.text))
+            return 'UNKNOWN TITLE'
+        channelId = req.json()['data'][0]['id']
+        req = requests.get('https://api.twitch.tv/helix/channels?broadcaster_id={}'.format(channelId), headers=headers)
+        if req.status_code != requests.codes.ok:
+            logging.warning('Failed to get channel title due to HTTP error. Code: {} | Text: {}'.format(req.status_code, req.text))
+            return 'UNKNOWN TITLE'
+        data = req.json()
+        title = data['data'][0]['title']
+        logging.info('Found Video Title: {}'.format(title))
+        return title
+    elif(mode == "kick"):
+        scraper = cloudscraper.create_scraper()
+        url = ("https://kick.com/api/v1/channels/bbjess")
+        req = scraper.get(url)
+        if req.status_code != requests.codes.ok:
+            logging.warning('Failed to get channel title due to HTTP error. Code: {} | Text: {}'.format(req.status_code, req.text))
+            return 'UNKNOWN TITLE'
+        data = req.json()
+        if "session_title" in data['livestream']:
+            title = data['livestream']['session_title']
+            return(title)
+        else:
+            if (old != "") or (old != "UNKNOWN TITLE"):
+                return old
+            else:
+                logging.warning('Failed to get channel title due to API error. Code: {} | Text: {}'.format(req.status_code, req.text))
+                return 'UNKNOWN TITLE'
+    else:
+        logging.critical("Invalid mode argument please specify kick or twitch")
+        os._exit(1)
 
 def check_generate_path(pathPrefix):
     date = datetime.date.today()
@@ -92,9 +123,18 @@ if __name__ == '__main__':
         logging.info('Done.')
 
         try:
-            streams = sl.streams('https://twitch.tv/{}'.format(channelName))
+            if(mode == "twitch"):
+                streams = sl.streams('https://twitch.tv/{}'.format(channelName))
+            elif(mode == "kick"):
+                streams = sl.streams('https://kick.com/{}'.format(channelName))
+            else:
+                logging.critical("Invalid mode argument please specify kick or twitch")
+                os._exit(1)
         except streamlink.exceptions.PluginError:
             logging.error('Failed to fetch stream via streamlink.')
+            continue
+        except Exception as e:
+            logging.warning('Failed to record stream. Code: {}'.format(e))
             continue
         if not streams:
             logging.info('No streams are available.')
@@ -103,7 +143,7 @@ if __name__ == '__main__':
             logging.error('`best` stream not available!')
             break
         logging.info('Stream found! Opening ffmpeg...')
-
+        title = get_channel_title("")
         fullDownloadPath = '{}/{}.flv'.format(downloadPath, int(time.time()))
         logging.info('Writing download to: {}...'.format(fullDownloadPath))
         stream = ffmpeg.input(streams['best'].url).output(fullDownloadPath, vcodec='copy', acodec='aac')
@@ -113,7 +153,7 @@ if __name__ == '__main__':
 
         #check_generate_path(finalPath)
 
-        title = get_channel_title()
+        title = get_channel_title(title)
         validChars = "-.() %s%s" % (string.ascii_letters, string.digits)
         title = ''.join(c for c in title if c in validChars)
         logging.info('Modified Title: {}'.format(title))
